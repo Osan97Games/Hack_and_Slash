4dd9432d7ac1cf2168e502d78c1add883cd0fd35
Starbead <wnswld@gmail.com>
2022-07-10 23:20:56 +0900

    feat : Enemys movement

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,7 +5,43 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
+    private GameObject player;
+    [SerializeField]
     private int m_Damage      = 3;
     [SerializeField]
     private float m_Speed     = 2.5f;
+    [SerializeField]
+    private EnemyData data;
+
+    private void Start()
+    {
+        InitEnemyInfo();
+    }
+
+    private void Update()
+    {
+        MoveToPlayer();
+    }
+
+    private void MoveToPlayer()
+    {
+        this.transform.LookAt(player.transform.position);
+        this.transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, m_Speed * Time.deltaTime);
+    }
+
+    private void InitEnemyInfo()
+    {
+        //use EnemyData
+        //set -> data.hp
+        m_Damage = data.damage;
+        m_Speed = data.speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //attack player
+        }
+    }
 }
